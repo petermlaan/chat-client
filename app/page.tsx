89bc85e967220b89e090d730df8fbff2dfa47887
Peter Laan <plaan70@gmail.com>
2025-03-29 16:16:20 +0100

    splitter fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import styles from "./page.module.css"
 import PageC from "./pagec";
-import Rooms from "./rooms";
 
 export default function Page() {
   return (
     <div className={styles.page}>
-      <Rooms />
       <PageC />
     </div>
   )
