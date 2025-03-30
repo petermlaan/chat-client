ebad41857968571142e5564c28897547dc1975cf
Peter Laan <plaan70@gmail.com>
2025-03-30 09:58:42 +0200

    spam interval cleanup added

--- app/chatroom.tsx
+++ app/chatroom.tsx
@@ -3,6 +3,7 @@ import { rnd } from "@/lib/util"
 import { useChatContext } from "./chatcontext"
 import styles from "./chatroom.module.css"
 import Rooms from "./rooms"
+import { useState } from "react"
 
 const spam = [
     "SPAM!!!",
@@ -16,6 +17,7 @@ const spam = [
     "NOOOOOOO!!!!!!!! :(",
     "sure!",
     "What is this room for???",
+    "Chaticus Maximus caused the fall of the Roman Empire. They couldn't handle that many chats.",
 ]
 
 export default function ChatRoom() {
@@ -28,16 +30,22 @@ export default function ChatRoom() {
         }
     }
     function onBtnSpam() {
-/*         const node = document.querySelector("#delay")
-        if (node) {
-            const input = node as HTMLInputElement
-            const delay = 1000 * +input.value */
-            window.setInterval(() => {
+        /*         const node = document.querySelector("#delay")
+                if (node) {
+                    const input = node as HTMLInputElement
+                    const delay = 1000 * +input.value */
+        if (spamId > -1) {
+            window.clearInterval(spamId)
+            setSpamId(-1)
+        } else {
+            const id = window.setInterval(() => {
                 const m = spam[rnd(spam.length - 1)]
                 cc.sendMsg(m)
             }, 1000)
-//        }
+            setSpamId(id)
+        }
     }
+    const [spamId, setSpamId] = useState(-1)
     const cc = useChatContext()
 
     return (
