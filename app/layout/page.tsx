beb659376fabd745391bb6846b05d409e2c0e747
Peter Laan <plaan70@gmail.com>
2025-03-31 09:15:32 +0200

    LayoutPage changed

--- app/layout/page.tsx
+++ app/layout/page.tsx
@@ -1,10 +1,16 @@
+import styles from "./page.module.css"
 import EditLayout from "../editlayout"
 
 export default function LayoutPage() {
     return (
-        <div>
-            <h1>Layout Editor</h1>
-            <EditLayout />
+        <div className={styles.page}>
+            <h2>Layout Editor</h2>
+            <select>
+                <option>Layouts...</option>
+                <option>Layout 1</option>
+                <option>Layout 2</option>
+            </select>
+            <div className="flexcent"><EditLayout /></div>
         </div>
     )
 }
