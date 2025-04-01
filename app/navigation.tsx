a87855f4caa5b4baa010921ce79760dbe70c9c47
Peter Laan <plaan70@gmail.com>
2025-04-01 21:38:21 +0200

    layout quick menu

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,16 +1,31 @@
 "use client"
+import { useGlobalContext } from "@/components/layoutcontext"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { MouseEvent as ReactMouseEvent } from "react"
 
 export default function Navigation() {
-    return (
-        <nav className="nav">
-        <Link href="/" prefetch={false} className={`nav-link ${usePathname() === '/' ? 'active' : ''}`}>
-          Chat
-        </Link>
-        <Link href="/layout" prefetch={false} className={`nav-link ${usePathname() === '/layout' ? 'active' : ''}`}>
+  function onLayout(e: ReactMouseEvent<HTMLButtonElement, MouseEvent>) {
+    gc.setLayout(+e.currentTarget.value)
+  }
+
+  const gc = useGlobalContext()
+
+  return (
+    <nav className="nav">
+      <Link href="/" prefetch={false} className={`nav-link ${usePathname() === '/' ? 'active' : ''}`}>
+        Chat
+      </Link>
+      <div className="dropdown">
+        <Link href="/layout" prefetch={false} className={`dropbtn nav-link ${usePathname() === '/layout' ? 'active' : ''}`}>
           Layout
         </Link>
-      </nav>
-    )
+        <div className="dropdown-content">
+          {gc.layouts.layouts.map((l, i) =>
+            <button onClick={onLayout} value={l.id} key={i}>{l.name}</button>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
 }
