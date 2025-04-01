b975040295030ecccde7db2179671fe4a01e50d1
Peter Laan <plaan70@gmail.com>
2025-04-01 08:34:50 +0200

    chat page styling

--- app/chatroom.tsx
+++ app/chatroom.tsx
@@ -41,7 +41,7 @@ export default function ChatRoom() {
                     </svg>
                 </button>
                 <div className="flexcentgrow">
-                    <input type="text" id="msgtxt" />
+                    <input type="text" className="msgtxt" />
                     <button onClick={onBtnSend} className="imgbtn">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                             viewBox="0 0 24 24" fill="none" stroke="var(--foreground)" strokeWidth="2"
